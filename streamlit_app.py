8628b04a2ead777dc3ab072712bc21eb8935dfd4
S Joshi <32176601+sjoshi108@users.noreply.github.com>
2024-10-18 11:27:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,13 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title('Sleep streamlit Website')
-st.write('Analysis of sleep')
+st.title('Sleeeeeeeeep')
+st.write('Analysis of how health and lifestyle affects sleep')
 df = pd.read_csv('Sleep_health_and_lifestyle_dataset.csv')
 
-
-st.write('3D plot of 4 features')
+st.title('Hypothesis 1')
+st.write(' Higher stress levels are associated with poorer sleep quality and shorter sleep duration')
+st.write('The interaction between stress level and physical activity affects sleep duration and quality, moderated by age.')
 
 fig1 = px.scatter_3d(df, x='Physical_Activity_Level', y='Stress_Level', z='Sleep_Duration', 
                      color='Age', title="Interaction of Physical Activity, Stress Level, and Sleep Duration with Age",
