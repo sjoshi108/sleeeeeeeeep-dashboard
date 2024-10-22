19286b8e9cb5b0f30bb3f0c474a5255856aebf13
S Joshi <32176601+sjoshi108@users.noreply.github.com>
2024-10-22 09:56:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,4 +98,3 @@ elif page == "Conclusion":
     3. In the case of **rail workers**, job insecurity and work surges directly contribute to reduced sleep quality, highlighting the importance of addressing job-related stress.
     By addressing these factors, individuals and organizations can take steps toward improving sleep health and overall well-being.
     """)
-
